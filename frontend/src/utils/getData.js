7ae739c4aa16fa3d3a093b413c923189030b0a4e
pradeep4j <pradeep4j>
2023-04-24 19:11:04 +0530

    april 24

--- frontend/src/utils/getData.js
+++ frontend/src/utils/getData.js
@@ -3,8 +3,6 @@ import {http,getToken} from '../utils/localStorage';
 //const URL = 'http://localhost:5000/admin'; 
 const URL = 'http://gridxecosystem.in:5000/admin'; 
 const getData = async (props) => {
-
-//   const token = getCookie('gridx-client');
     const response = await axios.post(
         `${URL}/usersList`,
         props.cred,
@@ -15,14 +13,6 @@ const getData = async (props) => {
             withCredentials: true,
         },
     );
-	// const config = {
-    //     headers: {
-    //         "Content-Type":"application/json",
-    //         Authorization : `Bearer ${getToken()}`
-    //     }
-    // }
-    //alert(JSON.stringify(data)); return;
-    // return await axios.post(`${URL}/usersList`,data,config);
     return response;
 };
 
